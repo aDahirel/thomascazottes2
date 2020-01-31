aa3fe43167f7e9c2251cc15612aeabced33c9036
jeanlagniaux <34351725+jeanlagniaux@users.noreply.github.com>
2020-01-31 13:15:07 +0100

    juste need to change img disposition

--- public/assets/js/demo2.js
+++ public/assets/js/demo2.js
@@ -78,21 +78,6 @@
                     else {
                         TweenMax.set(this.DOM.imgs[this.current], {opacity: 1});
                     }
-                    
-                    TweenMax.staggerTo(letters, 0.2, {
-                        ease: Sine.easeInOut,
-                        y: this.lastMousePos.translation.y < this.mousePos.y ? 30 : -30,
-                        startAt: {opacity: 1, y: 0},
-                        opacity: 0,
-                        yoyo: true,
-                        yoyoEase: Back.easeOut,
-                        repeat: 1,
-                        stagger: {
-                            grid: [1,letters.length-1],
-                            from: 'center',
-                            amount: 0.12
-                        }
-                    });
                 };
                 item.addEventListener('mouseenter', mouseenterFn);
             });
@@ -112,7 +97,7 @@
             this.lastMousePos.displacement.x = lerp(this.lastMousePos.displacement.x, this.mousePos.x, 0.1);
             this.lastMousePos.displacement.y = lerp(this.lastMousePos.displacement.y, this.mousePos.y, 0.1);
             const mouseDistance = distance(this.lastMousePos.displacement.x, this.mousePos.x, this.lastMousePos.displacement.y, this.mousePos.y);
-            this.dmScale = Math.min(mouseDistance, 100);  
+            this.dmScale = Math.min(mouseDistance, 100);
             feDisplacementMapEl.scale.baseVal = this.dmScale;
 
             requestAnimationFrame(() => this.render());
