1f0d3bc9ee48029b42a2445ea546d57c39733457
jeanlagniaux <34351725+jeanlagniaux@users.noreply.github.com>
2020-03-20 22:53:03 +0100

    add new image and test it
    A FAIRE : changer le modal pour pouvoir empecher le scroll en background et peut etre trouver un meilleur moyen d'ouvrire la grande image

--- public/assets/js/modal.js
+++ public/assets/js/modal.js
@@ -1,6 +1,5 @@
 // Get the modal
 var modal = document.getElementById('myModal');
-
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = $('.myImg');
 var modalImg = $("#img01");
@@ -8,9 +7,14 @@ var captionText = document.getElementById("caption");
 $('.myImg').click(function(){
     modal.style.display = "block";
     var newSrc = this.src;
-    newSrc = newSrc.replace('.jpg', 'Grand.png');
+    newSrc = newSrc.replace('.jpg', 'Grand.jpg');
     modalImg.attr('src', newSrc);
     captionText.innerHTML = this.alt;
+
+    // TODO trouver une soluttion pour empecher scroll en arriere plan quand le modal est ouvert. 
+
+    //document.getElementById("bodyIden").style.background = "red";
+    //document.getElementById("bodyIden").style.overflow = "hidden";
 });
 
 // Get the <span> element that closes the modal
