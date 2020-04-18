a76549e8a7c02783bbc0f19994b7be006e8db8aa
jeanlagniaux <34351725+jeanlagniaux@users.noreply.github.com>
2020-04-18 12:37:08 +0200

    fonction prev and next in gallery

--- public/assets/js/modal.js
+++ public/assets/js/modal.js
@@ -4,10 +4,10 @@ var modal = document.getElementById('myModal');
 var img = $('.myImg');
 var modalImg = $("#img01");
 var captionText = document.getElementById("caption");
-$('.myImg').click(function(){
+$('.myImg').click(function () {
     modal.style.display = "block";
     var newSrc = this.src;
-    var modalSrc = newSrc.replace('MIGNA', 'NATIF') ;
+    var modalSrc = newSrc.replace('MIGNA', 'NATIF');
     modalImg.attr('src', modalSrc);
     captionText = this.alt;
 
@@ -17,8 +17,58 @@ $('.myImg').click(function(){
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var prevClick = document.getElementsByClassName("prev")[0];
+var nextClick = document.getElementsByClassName("next")[0];
+
+nextClick.onclick = function () {
+    var src = "", srcNext = "", firstDigit = "", numDigit = "", theNextSrc = "";
+    modal.style.display = "block";
+    src = $("#img01");
+    srcNext = src[0].src.match(/\/(\w*)\/(SITE).*$/);
+    var regexp = /[0-9]{1,2}/g;
+    var matches = srcNext[0].matchAll(regexp);
+    var arraymatch = [];
+    for (const match of matches) {
+        arraymatch.push(match)
+    }
+    firstDigit = parseInt(arraymatch[0][0]);
+    firstDigit = firstDigit + 1;
+    numDigit = parseInt(arraymatch[1][0]);
+    if (firstDigit <= numDigit) {
+        theNextSrc = srcNext[0];
+        theNextSrc = theNextSrc.replace(/[0-9]{1,2}/, firstDigit);
+        modalImg.attr('src', theNextSrc);
+    } else {
+        console.log('no more media to display');
+        modal.style.display = "none";
+    }
+};
+
+prevClick.onclick = function () {
+    var src = "", srcPrev = "", firstDigit = "", numDigit = "", thePrevSrc = "";
+    modal.style.display = "block";
+    src = $("#img01");
+    srcPrev = src[0].src.match(/\/(\w*)\/(SITE).*$/);
+    var regexp = /[0-9]{1,2}/g;
+    var matches = srcPrev[0].matchAll(regexp);
+    var arraymatch = [];
+    for (const match of matches) {
+        arraymatch.push(match)
+    }
+    firstDigit = parseInt(arraymatch[0][0]);
+    firstDigit = firstDigit - 1;
+    numDigit = parseInt(arraymatch[1][0]);
+    if (firstDigit !== 0) {
+        thePrevSrc = srcPrev[0];
+        thePrevSrc = thePrevSrc.replace(/[0-9]{1,2}/, firstDigit);
+        modalImg.attr('src', thePrevSrc);
+    } else {
+        console.log('no more media to display');
+        modal.style.display = "none";
+    }
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+span.onclick = function () {
+    modal.style.display = "none";
+};
